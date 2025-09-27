3fcaf0de1f8c89a0632dbfc0826bbc4367fb9401
cotswoldsmaker <mark.allan.bailey@gmail.com>
2025-09-27 19:49:46 +0100

    feat: add security rules to Semgrep configuration and implement tests for dangerous patterns

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -6,6 +6,9 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import storybook from "eslint-plugin-storybook";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import regexp from "eslint-plugin-regexp";
+import react from "eslint-plugin-react";
+import noSecrets from "eslint-plugin-no-secrets";
 
 export default tseslint.config(
   [
@@ -25,6 +28,21 @@ export default tseslint.config(
       languageOptions: {
         ecmaVersion: 2020,
         globals: globals.browser,
+      },
+      plugins: { regexp, react, "no-secrets": noSecrets },
+      rules: {
+        "regexp/no-super-linear-backtracking": "warn",
+        "regexp/no-useless-quantifier": "warn",
+
+        // New: React XSS guard
+        "react/no-danger": "warn",
+
+        // Core JS “don’t shoot your foot” (no extra deps)
+        "no-eval": "error",
+        "no-implied-eval": "error",
+        "no-new-func": "error",
+
+        "no-secrets/no-secrets": ["warn", { tolerance: 4.5 }],
       },
     },
   ],
